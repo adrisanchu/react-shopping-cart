cbaa089ab519ae185af700ccfbfb6eb2d454c211
adrisanchu <adrian.sanchez@constant-urban.eu>
2021-02-02 09:46:20 +0100

    Group arguments in counter object and use it from the component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     // use props to pass values to the component !
-    value: this.props.value,
+    value: this.props.counter.value,
     // tags: ["tag1", "tag2", "tag3"],
     tags: [],
   };
@@ -46,7 +46,7 @@ class Counter extends Component {
           -
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
